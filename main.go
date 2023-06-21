2f9c55e37a739d318ac649cb7bd3686b54372d9f
jaskaransinghdr6j <39591298+jaskaransinghdr6j@users.noreply.github.com>
2023-06-20 21:07:53 -0700

    Update main.go

--- main.go
+++ main.go
@@ -5,8 +5,7 @@ import "fmt"
 func main() {
   secret_key := "this_is-secret"
   fmt.println(secret_key)
-  fmt.println("ping")
-  fmt.println("yup)
+c  fmt.println("yup)
   fmt.println("check agaon")
                 fmt.printkn("and again")
                 fmt.printkn("and again")
@@ -19,6 +18,7 @@ func main() {
          v3rv       fmt.printkn("and again")
                 fmt.printkn("and again")
   fmt.printkn("and again")
+  fmt.println("check agaon")
   fmt.println("check agaon")
 
 }
