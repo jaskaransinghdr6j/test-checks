49f3f0e50c7993d6db5b1ec5a09fa05f4d242ada
jaskaransinghdr6j <39591298+jaskaransinghdr6j@users.noreply.github.com>
2023-06-20 21:30:32 -0700

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ c  fmt.erprintln("yup)oj
 Findings.Link = common.ConstructDefectDojoFindingsUrl(dd_product.ID)                fmt.printkn("and again")
 38826658                fmt.printkn("and again")
                 fmt.printkn("and again")
-  fmtmk;.printkn("and again")
+  fmtmk;.printkcrn("and again")
                 fmt.printkn("and again")
                 fmt.printkn("and again")
          v3rv       fmt.printkn("and again")
